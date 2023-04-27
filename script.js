38262d0c47e4667b91b6b8193a16987e232293eb
Wesley Aguiar <aguiar.softeng@gmail.com>
2023-04-27 13:50:17 -0300

    Corrigindo identação do código

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const pipe = document.querySelector(".pipe");
 const clouds = document.querySelector(".nuvens")
 const points = document.querySelector(".points")
 
-let score = 0
-
 const jump = () => {
     mario.classList.add("jump");
 
@@ -18,8 +16,6 @@ const loop = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "")
     const cloudsPosition = +window.getComputedStyle(clouds).left.replace("px", "")
 
-    console.log(cloudsPosition)
-
     if (pipePosition <= 90 && pipePosition > 0 && marioPosition < 80){
         pipe.style.animation = "none"
         pipe.style.left = `${pipePosition}px`
@@ -31,10 +27,7 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`
         mario.setAttribute("src", "Imagens/lose.png")
         mario.setAttribute("class", "lose")
-        score += 1
     }
 }, 10)
-
-points.innerHTML = `Score: ${score}`
 
 document.addEventListener("keydown", jump);
